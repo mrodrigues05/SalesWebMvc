d76ad0e3d0c7d00a36c7ad11b7959f460a97fadf
Matheus Rodrigues <mmrodrigues077@gmail.com>
2023-08-18 13:12:14 -0300

    First controller and razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            ViewData["email"] = "matheusrodrigues9248@gmail.com";
             return View();
         }
 
